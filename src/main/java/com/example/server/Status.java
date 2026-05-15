7d0586459f76d9df21de7591c33e3d12335d34e2
K-OcO-M <kcmit26@gmail.com>
2026-05-15 14:32:50 +0900

    sever memory endpoint 구현

---
+++ src/main/java/com/example/server/Status.java
@@ -0,0 +1,31 @@
+package com.example.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Status")
+public class Status {
+
+	@GetMapping("/Check")
+	public String Check() {
+		return "체크";
+	}
+
+	// 메모리 return(반환) 해주는 코드 작성할 것
+	@GetMapping("/Memory")
+	public String Memory() {
+		Runtime rt = Runtime.getRuntime();
+
+		long totalMemory = rt.totalMemory() / 1024 / 1024; // 전체 메모리
+		long freeMemory = rt.freeMemory() / 1024 / 1024; // 남은 메모리
+		long usedMemory = totalMemory - freeMemory; // 사용중 메모리
+		long maxMemory = rt.maxMemory() / 1024 / 1024; // 최대 메모리
+
+		return "Total: " + totalMemory + "MB" + " | Used: " + usedMemory + "MB" + " | Free: " + freeMemory + "MB"
+				+ " | Max: " + maxMemory + "MB";
+
+	}
+
+}
