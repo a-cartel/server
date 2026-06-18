f0d3265f769c0895a6af329636cd462436ae2d12
K-OcO-M <kcmit26@gmail.com>
2026-06-18 15:59:57 +0900

    refactor: auth DTO Lombok 적용 및 pom 수정

--- src/main/java/com/example/server/auth/dto/LoginRequest.java
+++ src/main/java/com/example/server/auth/dto/LoginRequest.java
@@ -1,23 +1,14 @@
 package com.example.server.auth.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class LoginRequest {
 
-    private String email;
-    private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	private String email;
+	private String password;
 }
--- src/main/java/com/example/server/auth/dto/LoginResponse.java
+++ src/main/java/com/example/server/auth/dto/LoginResponse.java
@@ -1,26 +1,13 @@
 package com.example.server.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginResponse {
 
 	private String message;
 	private boolean login;
 	private String email;
-
-	public LoginResponse(String message, boolean login, String email) {
-		this.message = message;
-		this.login = login;
-		this.email = email;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public boolean isLogin() {
-		return login;
-	}
-
-	public String getEmail() {
-		return email;
-	}
 }
